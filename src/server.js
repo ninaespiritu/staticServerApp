e46dd19ffde8786724c150b24fc2a0ebf6742e4c
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-02-14 12:22:44 +0000

    Fix routes for multiple pages

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 5001;
 
 app.use("/static", express.static("public"));
+app.use("/about", express.static("public/pages/about.html"));
+app.use("/portfolio", express.static("public/pages/portfolio.html"));
+app.use("/contact", express.static("public/pages/contact.html"));
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
